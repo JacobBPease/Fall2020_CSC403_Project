ab2eb6c27322fd85438e87fe1dfee3a78758999d
TramD <tramdoan25@gmail.com>
2020-11-12 11:35:55 -0600

    gave battle characters items, inventory, and ability to add item

--- Project/Fall2020_CSC403_Project/code/BattleCharacter.cs
+++ Project/Fall2020_CSC403_Project/code/BattleCharacter.cs
@@ -5,42 +5,67 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Fall2020_CSC403_Project.code {
-  public class BattleCharacter : Character {
-    public int Health { get; private set; }
-    public int MaxHealth { get; private set; }
-    public int Armor;
-    public float strength;
+    public class BattleCharacter : Character {
+        public int Health { get; private set; }
+        public int MaxHealth { get; private set; }
+        public int Armor;
+        public float strength;
 
-    public event Action<int> AttackEvent;
+        public event Action<int> AttackEvent;
 
-    public BattleCharacter(Vector2 initPos, Collider collider) : base(initPos, collider) {
-      MaxHealth = 20;
-      strength = 2;
-      Armor = 3;
-      Health = MaxHealth;
-    }
+        // list of possible items
+        private List<string> items = new List<string>()
+     {
+            "Potion", "Potion", "Regular Staff", "Regular Sword",
+            "Fire Sword", "Lightning Sword", "Magic Staff"
+     };
 
-    public void OnAttack(int amount) {
-      AttackEvent((int)(amount * strength));
-    }
+        // list of items in inventory
+        public List<string> inInventory = new List<string>()
+     {
+         "Potion", "Potion"
+     };
 
-    public void AlterHealth(int amount) {
-      Health += amount;
-    }
+
+
+        public BattleCharacter(Vector2 initPos, Collider collider) : base(initPos, collider) {
+            MaxHealth = 20;
+            strength = 2;
+            Armor = 3;
+            Health = MaxHealth;
+        }
+
+        public void OnAttack(int amount) {
+            AttackEvent((int)(amount * strength));
+        }
+
+        public void AlterHealth(int amount) {
+            Health += amount;
+        }
 
 
         public void ElementalPower(bool fire, bool magic, bool lightning) {
-      if (fire) {
-        strength += 2; // add fire damage
-      }
-      if (magic) {
-        strength += 2; // add magic damage and give the player extra health
-        AlterHealth(2);
-      }
-      if (lightning) {
-        strength += 2; // add lightning damage
-        Armor++;
-      }
-    }
-  }
+            if (fire) {
+                strength += 2; // add fire damage
+            }
+            if (magic) {
+                strength += 2; // add magic damage and give the player extra health
+                AlterHealth(2);
+            }
+            if (lightning) {
+                strength += 2; // add lightning damage
+                Armor++;
+            }
+
+        }
+
+        public void addItem()
+        {
+            //Randomly choose item to add
+            Random random = new Random();
+            int randomNumber = random.Next(items.Count);
+            inInventory.Add(items[randomNumber]);
+            items.RemoveAt(randomNumber);
+        }
+    } 
 }
